c7d58222befbefa18b6b88cfc99bbbdccc38c1a7
Oshinowo Abdulazeez <abdulazeezoshinowo2@gmail.com>
2021-02-24 10:22:42 +0100

    initial commit

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,39 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+
+  List<Question> _questionBank = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('The Nigerian flag has a red colour', false),
+    Question('I can write flutter very well', true)
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  String getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
